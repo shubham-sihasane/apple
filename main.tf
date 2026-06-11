330d633c7d8695d36171026b5101fab018261a1f
Shubham Sihasane <78297361+shubham-sihasane@users.noreply.github.com>
2026-06-11 22:56:40 +0000

    Simplify aws_instance resource definition
    Removed unnecessary tags and comments for clarity.

--- main.tf
+++ main.tf
@@ -2,9 +2,6 @@
 resource "aws_instance" "devops_machine" {
   ami           = "ami-07a00cf47dbbc844c"
   instance_type = "t3.micro"
-  tags = {
-    "Name" = "DevOps Machine"
-  }
 }
 
 # resource "github_repository" "example" {
